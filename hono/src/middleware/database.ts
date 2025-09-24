93545c8cdbe3e07f298413ffe92b735ee355041c
Mukesh Yadav <mak.gnu@gmail.com>
2025-09-25 01:53:38 +0530

    some changes

--- hono/src/middleware/database.ts
+++ hono/src/middleware/database.ts
@@ -7,6 +7,8 @@ import * as schema from '../db/schema'
 export const getDb = (): MiddlewareHandler => {
     return createMiddleware<Env>(async (ctx: Context, next: Next) => {
         if (!ctx.get('db')) {
+            console.log('Creating database connection', `${ctx}`)
+            console.log('Creating database connection', `${ctx.env.TURSO_DATABASE_URL} ${ctx.env.TURSO_AUTH_TOKEN}`)
             const client = createClient({
                 url: ctx.env.TURSO_DATABASE_URL,
                 authToken: ctx.env.TURSO_AUTH_TOKEN,
